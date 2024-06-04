9566da2d590ef8f2aba35e64dd41ddec7519068f
Pushpendra <pushpendra616singh@gmail.com>
2024-06-04 18:42:25 +0530

    shift mongoDB to online server

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // define the mongodb connection URL
 
-const mongoURL = "mongodb://127.0.0.1/hotels";
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 // set up MongoDb connection
 mongoose.connect(mongoURL, {
--- server.js
+++ server.js
@@ -91,8 +91,10 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body // JSON data ko parse karne ke liye middleware
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("wlcome to my hotel... How can i help you?, we have list of menus");
@@ -107,6 +109,6 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/person", personRoutes); // /person this is here becouse we removed from personRoutes.js file
 app.use("/menu", menuItemRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 3000...");
 });
